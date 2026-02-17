70e839a988e1b304238028095e7b220746716cfb
Asylbek <akylbekovasylbek07@gmail.com>
2026-02-17 21:57:05 +0600

    Обновил папку продукт

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,5 +1,19 @@
-from django.views.generic import TemplateView
-from apps.product.models import Category, Product
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.shortcuts import get_list_or_404, redirect
+from django.urls import reverse_lazy
+from django.db.models import Prefetch
+
+from apps.product.models import (
+    Category, Product,
+    ProductVariant, Review)
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'product/category_list.html'
+    context_object_name = 'categories'
+
+    def get_queryset(self):
+        return Category.objects.filter(is_active=True, parent__isnull=True)
 
 class HomeView(TemplateView):
     template_name = 'index.html'
